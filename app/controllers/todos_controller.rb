db7557bec554c79214260638817460e5a7057688
Chris Taylor <ctaylor534@gmail.com>
2022-08-15 11:27:09 -0700

    Finishes testing of authetication

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,15 +1,16 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
-  # GET /todos
   def index
-    @todos = Todo.all
+    # get current user todos
+    @todos = current_user.todos
     json_response(@todos)
   end
 
   # POST /todos
   def create
-    @todo = Todo.create!(todo_params)
+    # create todos belonging to current user
+    @todo = current_user.todos.create!(todo_params)
     json_response(@todo, :created)
   end
 
@@ -33,8 +34,7 @@ class TodosController < ApplicationController
   private
 
   def todo_params
-    # whitelist params
-    params.permit(:title, :created_by)
+    params.permit(:title)
   end
 
   def set_todo
